e865eeb4e46dc21ceb69074554a336d0cb7d6379
David Leyva <dleyvaabrahantes@gmail.com>
2023-09-06 14:52:15 -0400

    Initial Commit

---
+++ SignupValidation/SignupValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignupValidationApp.swift
+//  SignupValidation
+//
+//  Created by David on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SignupValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
